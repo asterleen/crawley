7836913f2138228910901f95645400c906ab8941
Akito Iwakura <aki@asterleen.com>
2018-10-31 14:04:27 +0300

    Fix temporary config module

--- src/engine/tempconfig.php
+++ src/engine/tempconfig.php
@@ -10,6 +10,8 @@
 $config = Array();
 
 function config_load() {
+	global $config;
+
 	$content = file_get_contents('engine/runtime/config.json');
 
 	if (!empty($content)) {
@@ -21,14 +23,22 @@ function config_load() {
 }
 
 function config_save() {
-	file_put_contents('engine/runtime/config.json', $config);
+	global $config;
+
+	file_put_contents('engine/runtime/config.json', json_encode($config));
 }
 
 function config_getVal($key, $defaultValue = 0) {
+	global $config;
+
 	return empty($config[$key]) ? $defaultValue : $config[$key];
 }
 
 function config_setVal($key, $value) {
+	global $config;
+
 	$config[$key] = $value;
 	config_save();
 }
+
+config_load();
